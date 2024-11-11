2df618e4eb309dd4c6b8491e9c4417b4e733223b
Johan Méndez <johanmendezh@habi.co>
2024-11-11 15:30:28 -0500

    [ADD] some features to main API

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import router
 
+app = FastAPI(
+    title="Magneto Job",
+    description="API to find out if it's an X men",
+    version="0.0.1 - Born a Hero"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.include_router(router)
+# uvircorn main:app --reload
