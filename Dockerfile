a72801d22ebc495a7d319a92d359e8c07b3db24b
Johan Méndez <johanmendezh@habi.co>
2024-11-11 22:34:58 -0500

    [UPDATE] things to deploy

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Expose the port to FastAPI
-EXPOSE 8000
+EXPOSE 8080
 
 # Run!
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import router
@@ -19,4 +21,8 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvircorn.run("app.main:app", host="0.0.0.0", port=port)
 # uvircorn main:app --reload
