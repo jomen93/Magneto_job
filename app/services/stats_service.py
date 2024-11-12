053f056f06e6f0297b77a363f4c86bad6d5284d6
Johan Méndez <johanmendezh@habi.co>
2024-11-12 10:06:17 -0500

    [FIX] ratio formula

--- app/services/stats_service.py
+++ app/services/stats_service.py
@@ -12,7 +12,7 @@ def get_stats(db: Session):
         DNASequence.is_mutant == False
     ).count()
 
-    ratio = number_mutant_dna / number_mutant_dna if number_human_dna > 0 else 0
+    ratio = number_mutant_dna / number_human_dna if number_human_dna > 0 else 0
 
     return {
         "count_mutant_dna": number_mutant_dna,
