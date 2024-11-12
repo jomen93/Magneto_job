679e9ee4db2f1ed9f3db8fedd8bc3b9b03809429
Johan Méndez <johanmendezh@habi.co>
2024-11-12 10:06:28 -0500

    [ADD] Welcome page

--- app/routers.py
+++ app/routers.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from app.api.routes import mutant, stats
+from fastapi.templating import Jinja2Templates
+
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def read_welcome(request: Request):
+    return templates.TemplateResponse("welcome.html", {"request": request})
 
 router.include_router(mutant.router, prefix="/mutant", tags=["mutant"])
 router.include_router(stats.router, prefix="/stats", tags=["stats"])
