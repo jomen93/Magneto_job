aa1fecbc7ac9188d9469c80d9a342bd07934a165
Johan Méndez <johanmendezh@habi.co>
2024-11-12 09:36:57 -0500

    [UPDATE] get the statistics from prod db

--- app/api/routes/stats.py
+++ app/api/routes/stats.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from app.services.stats_service import get_stats
+from sqlalchemy.orm import Session
+from app.core.database import get_db
 
 
 router = APIRouter()
 
-
 @router.get("/", name="Statistical info", description="Related with data") 
-async def get_statistics():
+async def get_statistics(db: Session = Depends(get_db)):
     stats = get_stats()
     return stats
--- app/services/stats_service.py
+++ app/services/stats_service.py
@@ -1,9 +1,19 @@
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.models.dna_sequence import DNASequence
 
+def get_stats(db: Session):
 
-def get_stats():
-    number_mutant_dna = 40
-    number_human_dna = 100
-    ratio = number_mutant_dna / number_mutant_dna
+    number_mutant_dna = db.query(DNASequence).filter(
+        DNASequence.is_mutant == True
+    ).count()
+
+    number_human_dna = db.query(DNASequence).filter(
+        DNASequence.is_mutant == False
+    ).count()
+
+    ratio = number_mutant_dna / number_mutant_dna if number_human_dna > 0 else 0
+
     return {
         "count_mutant_dna": number_mutant_dna,
         "count_human_dna": number_human_dna,
